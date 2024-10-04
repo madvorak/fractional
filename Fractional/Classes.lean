badcd02922d62b5248dde151bd669e5d224eac6b
madvorak <martin.dvorak@matfyz.cz>
2024-10-04 17:31:12 +0200

    to not need to specify the coercion

--- Fractional/Classes.lean
+++ Fractional/Classes.lean
@@ -5,8 +5,8 @@ class Fragma (α : Type) [Fintype α] where
   op : FOP₂ α
 
 infix:65 " ⬙ " => Fragma.op
-infix:65 " ⬘ " => Fragma.op⌞
+infix:65 " ⬘ " => Fragma.op.apply₂
 
 theorem Fragma.op_eq {α : Type} [Fintype α] [DecidableEq α] [Fragma α] (a b : α) :
-    a ⬙ b = (a : Distr α) ⬘ b :=
+    a ⬙ b = a ⬘ b :=
   FOP₂.apply₂_coe Fragma.op a b
