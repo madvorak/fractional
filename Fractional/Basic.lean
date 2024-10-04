96385605e3b4ad44a2bf08690809e902b841f30b
madvorak <martin.dvorak@matfyz.cz>
2024-10-04 16:18:49 +0200

    singleton coercion

--- Fractional/Basic.lean
+++ Fractional/Basic.lean
@@ -18,14 +18,17 @@ variable {α : Type} [Fintype α]
 instance : CoeFun (Distr α) (fun _ => α → ℚ≥0) where
   coe := Distr.theFun
 
-def FOP₁.apply₁ (f : FOP₁ α) (x : Distr α) : Distr α where
+instance [DecidableEq α] : Coe α (Distr α) where
+  coe a := ⟨_, Fintype.sum_ite_eq a 1⟩
+
+abbrev FOP₁.apply₁ (f : FOP₁ α) (x : Distr α) : Distr α where
   theFun (a : α) := ∑ i : α, x i * f i a
   sumOne := by
     rw [Finset.sum_comm]
     conv => lhs; congr; rfl; ext; rw [←Finset.mul_sum, Distr.sumOne, mul_one]
     rw [Distr.sumOne]
 
-def FOP₂.apply₂ (f : FOP₂ α) (x y : Distr α) : Distr α where
+abbrev FOP₂.apply₂ (f : FOP₂ α) (x y : Distr α) : Distr α where
   theFun (a : α) := ∑ i : α, ∑ j : α, x i * y j * f i j a
   sumOne := by
     rw [Finset.sum_comm]
@@ -39,6 +42,7 @@ notation:max f"⌞" => FOP₁.apply₁ f
 variable {f : FOP₂ α} in
 notation:max f"⌞" => FOP₂.apply₂ f
 
+/-- `f⌞ x y = (f y)⌞ x` -/
 theorem FOP₂.apply₂_eq_apply₁_apply₁ (f : FOP₂ α) (x y : Distr α) :
     f⌞ x y = (fun i : α => (f i)⌞ y)⌞ x := by
   ext
@@ -47,3 +51,15 @@ theorem FOP₂.apply₂_eq_apply₁_apply₁ (f : FOP₂ α) (x y : Distr α) :
   ext
   rw [NNRat.coe_inj, Finset.mul_sum]
   simp_rw [mul_assoc]
+
+/-- `↑(f a) = f⌞ ↑a`  -/
+theorem FOP₁.apply₁_coe [DecidableEq α] (f : FOP₁ α) (a : α) :
+    f a = f⌞ a := by
+  ext i
+  by_cases hf : f a = i <;> simp [hf]
+
+/-- `f a b = f⌞ ↑a ↑b`  -/
+theorem FOP₂.apply₂_coe [DecidableEq α] (f : FOP₂ α) (a b : α) :
+    f a b = f⌞ a b := by
+  ext i
+  by_cases hf : f a b = i <;> simp [hf]
--- Fractional/Classes.lean
+++ Fractional/Classes.lean
@@ -4,4 +4,9 @@ import Fractional.Basic
 class Fragma (α : Type) [Fintype α] where
   op : FOP₂ α
 
-infix:65 " ◇ " => Fragma.op⌞
+infix:65 " ⬙ " => Fragma.op
+infix:65 " ⬘ " => Fragma.op⌞
+
+theorem Fragma.op_eq {α : Type} [Fintype α] [DecidableEq α] [Fragma α] (a b : α) :
+    a ⬙ b = (a : Distr α) ⬘ b :=
+  FOP₂.apply₂_coe Fragma.op a b
