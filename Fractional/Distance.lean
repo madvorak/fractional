0be0fa0bbac8eb36e424b76665edc404bb0b8823
madvorak <martin.dvorak@matfyz.cz>
2024-10-04 15:33:59 +0200

    distance done right

--- Fractional/Distance.lean
+++ Fractional/Distance.lean
@@ -3,63 +3,25 @@ import Fractional.Basic
 
 variable {α : Type} [Fintype α]
 
-def Distr.similarity (x y : Distr α) : ℚ≥0 :=
-  ∑ i : α, min (x i) (y i)
-
-lemma Distr.similarity_le_one (x y : Distr α) :
-    x.similarity y ≤ 1 := by
-  sorry
-
-lemma Distr.similarity_eq_one_iff (x y : Distr α) :
-    x.similarity y = 1 ↔ x = y := by
-  constructor <;> intro hxy
-  · sorry
-  · unfold Distr.similarity
-    simp_rw [hxy, min_self]
-    apply Distr.sumOne
-
-lemma Distr.similarity_comm (x y : Distr α) :
-    x.similarity y = y.similarity x := by
-  unfold Distr.similarity
-  apply congr_arg
-  ext
-  apply min_comm
-
-def Distr.distance (x y : Distr α) : ℚ≥0 :=
-  1 - x.similarity y
-
-lemma Distr.distance_le_one (x y : Distr α) :
-    x.distance y ≤ 1 := by
-  apply tsub_le_self
-
-lemma Distr.distance_eq_zero_iff (x y : Distr α) :
-    x.distance y = 0 ↔ x = y := by
-  convert Distr.similarity_eq_one_iff x y using 1
-  unfold Distr.distance
-  constructor <;> intro hxy
-  · sorry
-  · rw [hxy]
-    exact tsub_eq_zero_of_le rfl
-
-lemma Distr.distance_self (x : Distr α) :
-    x.distance x = 0 := by
-  rw [Distr.distance_eq_zero_iff]
-
-lemma Distr.distance_comm (x y : Distr α) :
-    x.distance y = y.distance x := by
-  unfold Distr.distance
-  rw [Distr.similarity_comm]
-
-theorem Distr.distance_eq (x y : Distr α) :
-    x.distance y = (∑ i : α, |(x i).val - (y i).val|) / 2 := by
-  simp only [distance, similarity, NNRat.val_eq_cast]
-  simp_rw [←max_sub_min_eq_abs]
-  rw [Finset.sum_sub_distrib]
-  sorry
-
-instance : MetricSpace (Distr α) where
-  dist x y := Distr.distance x y
-  dist_self x := sorry -- Distr.distance_self x
-  dist_comm x y := sorry -- Distr.distance_comm x y
-  dist_triangle x y z := sorry
-  eq_of_dist_eq_zero hxy := sorry -- Distr.distance_eq_zero_iff.mpr hxy
+noncomputable instance : MetricSpace (Distr α) where
+  dist x y :=
+    (∑ i : α, |(x i).val - (y i).val|) / 2
+  dist_self x := by
+    simp [dist]
+  dist_comm x y := by
+    simp [dist, abs_sub_comm]
+  dist_triangle x y z := by
+    simp [dist]
+    rw [←add_div, ←Finset.sum_add_distrib]
+    apply div_le_div_of_nonneg_right
+    swap; norm_num
+    apply Finset.sum_le_sum
+    intros
+    apply abs_sub_le
+  eq_of_dist_eq_zero hxy := by
+    simp [dist] at hxy
+    rw [Finset.sum_eq_zero_iff_of_nonneg (fun _ _ => abs_nonneg _)] at hxy
+    ext i
+    specialize hxy i (Finset.mem_univ i)
+    rw [abs_eq_zero, sub_eq_zero] at hxy
+    simp_all
