4bb12c36a8aa03971d0f9645a1a75c0c0cdc2819
madvorak <martin.dvorak@matfyz.cz>
2024-10-07 13:10:04 +0200

    without the notion of PreDistr

--- Fractional/Distance.lean
+++ Fractional/Distance.lean
@@ -45,94 +45,82 @@ lemma dist_le_dist_iff (u v x y : 𝍖 α) : u 𝄩 v ≤ x 𝄩 y ↔ ∑ i : �
 lemma FOP₁.app₁_dist_app₁_eq_zero (f : FOP₁ α) {x y : 𝍖 α} (hxy : x 𝄩 y = 0) : f⌞ x 𝄩 f⌞ y = 0 := by
   simp_all
 
-@[ext]
-private structure PreDistr (α : Type) [Fintype α] : Type where
-  theFun : α → ℝ
-  nonNeg : 0 ≤ theFun
+set_option linter.unusedSectionVars false
 
-notation "𝌅" => PreDistr
+private noncomputable def commonDistr (x y : α → ℝ) : α → ℝ :=
+  fun i : α => min (x i) (y i)
 
-private instance : CoeFun (𝌅 α) (fun _ => α → ℝ) where
-  coe := PreDistr.theFun
+private noncomputable def differDistr (x y : α → ℝ) : α → ℝ :=
+  fun i : α => if x i ≤ y i then 0 else x i - y i
 
-private instance : Coe (𝍖 α) (𝌅 α) where
-  coe x := ⟨x.theFun, x.nonNeg⟩
-
-private noncomputable def commonPreDistr (x y : 𝌅 α) : 𝌅 α where
-  theFun := fun i : α => min (x i) (y i)
-  nonNeg := fun i : α => le_min (x.nonNeg i) (y.nonNeg i)
-
-private noncomputable def differPreDistr (x y : 𝌅 α) : 𝌅 α where
-  theFun := fun i : α => if x i ≤ y i then 0 else x i - y i
-  nonNeg := fun i : α => by sorry
-
-private noncomputable def addPreDistr (x y : 𝌅 α) : 𝌅 α where
-  theFun := fun i : α => x i + y i
-  nonNeg := fun i : α => by sorry
-
-private lemma commonPreDistr_comm (x y : 𝌅 α) : commonPreDistr x y = commonPreDistr y x := by
+private lemma commonDistr_comm (x y : α → ℝ) : commonDistr x y = commonDistr y x := by
   ext
   apply min_comm
 
-private lemma add_common_differ (x y : 𝌅 α) : addPreDistr (commonPreDistr x y) (differPreDistr x y) = x := by
+private lemma add_common_differ (x y : α → ℝ) : commonDistr x y + differDistr x y = x := by
   ext i
-  by_cases hi : x i ≤ y i <;> simp [addPreDistr, commonPreDistr, differPreDistr, hi]
+  by_cases hi : x i ≤ y i <;> simp [commonDistr, differDistr, hi]
   rw [min_eq_right (le_of_not_ge hi)]
   apply add_sub_cancel
 
-private lemma add_common_differ' (x y : 𝍖 α) : addPreDistr (commonPreDistr x y) (differPreDistr x y) = (x : 𝌅 α) :=
-  add_common_differ (x : 𝌅 α) (y : 𝌅 α)
-
-private lemma ugly_sum (x y : 𝌅 α) :
+private lemma ugly_sum_ (x y : α → ℝ) :
     ∑ i : α,
-      |addPreDistr (commonPreDistr x y) (differPreDistr x y) i -
-       addPreDistr (commonPreDistr y x) (differPreDistr y x) i| =
+      |(commonDistr x y i + differDistr x y i) -
+       (commonDistr y x i + differDistr y x i)| =
     ∑ i : α,
-      |(differPreDistr x y) i - (differPreDistr y x) i| := by
+      |differDistr x y i - differDistr y x i| := by
   congr
-  ext
-  simp only [addPreDistr]
+  ext i
   apply congr_arg
-  rw [commonPreDistr_comm]
+  rw [commonDistr_comm]
   abel
 
-private lemma ugly_sum' (x y : 𝌅 α) :
-    ∑ i : α,
-      |(differPreDistr x y) i - (differPreDistr y x) i| =
-    ∑ i : α,
-      ((differPreDistr x y) i + (differPreDistr y x) i) := by
+private lemma ugly_sum' (x y : α → ℝ) :
+    ∑ i : α, |differDistr x y i - differDistr y x i| =
+    ∑ i : α, (differDistr x y i + differDistr y x i) := by
   congr
   ext i
   by_cases hi : x i ≤ y i
   · convert_to |0 - (y i - x i)| = (0 : ℝ) + (y i - x i) using 3
-    · simp [differPreDistr, hi]
-    · simp [differPreDistr, hi]
+    · simp [differDistr, hi]
+    · simp [differDistr, hi]
       intro
       linarith
-    · simp [differPreDistr, hi]
-    · simp [differPreDistr, hi]
+    · simp [differDistr, hi]
+    · simp [differDistr, hi]
       intro
       linarith
     rw [zero_add, zero_sub, abs_neg, abs_of_nonneg (sub_nonneg_of_le hi)]
-  · sorry
+  · convert_to |(x i - y i) - 0| = (x i - y i) + (0 : ℝ) using 3
+    · simp [differDistr, hi]
+    · simp [differDistr, hi]
+      intro
+      linarith
+    · simp [differDistr, hi]
+    · simp [differDistr, hi]
+      intro
+      linarith
+    rw [add_zero, sub_zero, abs_of_nonneg (sub_nonneg_of_le (le_of_not_ge hi))]
 
-private lemma less_ugly_sum (x y : 𝌅 α) :
+private lemma ugly_sum (x y : α → ℝ) :
     ∑ i : α,
-      |addPreDistr (commonPreDistr x y) (differPreDistr x y) i -
-       addPreDistr (commonPreDistr y x) (differPreDistr y x) i| =
+      |(commonDistr x y + differDistr x y) i -
+       (commonDistr y x + differDistr y x) i| =
     ∑ i : α,
-      ((differPreDistr x y) i + (differPreDistr y x) i) :=
-  (ugly_sum x y).trans (ugly_sum' x y)
+      (differDistr x y i + differDistr y x i) :=
+  Eq.trans
+    (ugly_sum_ x y)
+    (ugly_sum' x y)
 
 theorem FOP₁.app₁_dist_app₁_le_dist (f : FOP₁ α) (x y : 𝍖 α) : f⌞ x 𝄩 f⌞ y ≤ x 𝄩 y := by
   rw [dist_le_dist_iff]
-  have hx := add_common_differ' x y
-  have hy := add_common_differ' y x
-  have hd := hy ▸ hx ▸ less_ugly_sum (x : 𝌅 α) (y : 𝌅 α)
+  have hx := add_common_differ x y
+  have hy := add_common_differ y x
+  have hd := hy ▸ hx ▸ ugly_sum (x : α → ℝ) (y : α → ℝ)
   rw [hd]
-  have hx' := add_common_differ' (f⌞ x) (f⌞ y)
-  have hy' := add_common_differ' (f⌞ y) (f⌞ x)
-  have hd' := hy' ▸ hx' ▸ less_ugly_sum (f⌞ x : 𝌅 α) (f⌞ y : 𝌅 α)
+  have hx' := add_common_differ (f⌞ x) (f⌞ y)
+  have hy' := add_common_differ (f⌞ y) (f⌞ x)
+  have hd' := hy' ▸ hx' ▸ ugly_sum (f⌞ x : α → ℝ) (f⌞ y : α → ℝ)
   rw [hd']
   clear * -
   sorry
